dbebcc0b7092dc419b1aafe5fbf8115d15280de3
Cristi Dnla <cristi.dnla@gmail.com>
2024-08-10 00:32:19 +0300

    Add webflux sample code

---
+++ book-snippets/spring-start-here/ch11/webflux/src/main/java/com/example/webflux/entities/Payment.java
@@ -0,0 +1,11 @@
+package com.example.webflux.entities;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class Payment {
+    private String id;
+    private double amount;
+}
